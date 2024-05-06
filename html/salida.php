13cbc87407e5e4cca48248dc09821987b8ded59c
LESAcecyteh <lesa.cecyteh@gmail.com>
2024-05-05 21:53:53 -0600

    Add files via upload

---
+++ html/salida.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SALIDA</title>
+    <script src="C:\wamp64\www\proyectoint - copia\js\lesa(beta).js"type="text/javascript"></script>
+    <link rel="stylesheet" href="C:\wamp64\www\proyectoint - copia\assets\css\diseno.css">
+</head>
+<body>
+    <nav>
+
+        <input type="checkbox" id="toggle">
+          <div class="logo">
+              Lector de Entrada y Salida de Alumnos
+          </div>
+          <ul class="list">
+          <li><a href="C:\wamp64\www\proyectoint - copia\index.php">inicio</a></li>
+            <li><a href="C:\wamp64\www\proyectoint - copia\html\registro.php">Registro</a></li>
+            <li><a href="C:\wamp64\www\proyectoint - copia\html\ayuda.php">Ayuda</a></li>
+            <li><a href="C:\wamp64\www\proyectoint - copia\html\vista.php">Vista</a></li>
+          </ul>
+              <label for="toggle" class="icon-bars">
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+          </label>
+  
+  </nav>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+    <h1>Bienvenido al sistema de salida de alumnos</h1>
+    <h2>Para iniciar con el registro de salida de los alumnos se necesita de:</h2>
+    <ul>
+        <li><h3>Ultimos 3 digitos de la matricula del alumno</h3></li>
+    </ul>
+    
+</body>
+</html>
---
+++ html/salidalumno.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Copatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LESA</title>
+    <script src="C:\wamp64\www\proyectoint - copia\js\lesa(beta).js"type="text/javascript"></script>
+    <link rel="stylesheet" href="C:\wamp64\www\proyectoint - copia\assets\css\diseno.css">
+</head>
+<body>
+    <nav>
+
+        <input type="checkbox" id="toggle">
+          <div class="logo">
+              Lector de Entrada y Salida de Alumnos
+          </div>
+          <ul class="list">
+          <li><a href="C:\wamp64\www\proyectoint - copia\index.php">inicio</a></li>
+            <li><a href="C:\wamp64\www\proyectoint - copia\html\registro.php">Registro</a></li>
+            <li><a href="C:\wamp64\www\proyectoint - copia\html\ayuda.php">Ayuda</a></li>
+            <li><a href="C:\wamp64\www\proyectoint - copia\html\vista.php">Vista</a></li>
+          </ul>
+              <label for="toggle" class="icon-bars">
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+          </label>
+  
+  </nav>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+</body>
+</html>
