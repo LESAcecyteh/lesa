5791c69d560b94da15731a0cdd41f1297cabf827
LESAcecyteh <lesa.cecyteh@gmail.com>
2024-05-07 17:57:38 -0600

    Update lesa(beta).js

--- js/lesa(beta).js
+++ js/lesa(beta).js
@@ -9,18 +9,22 @@ function verificar() {
     alert('clave incorrecta');
     }
     }
+
 //SISTEMA DE ENTRADA    
 function entrada(){
     location.href="lectoren.html";
 }
+
 //SISTEMA DE SALIDA
 function salida(){
     location.href="lectorsal.html";
 }
+
 //SISTEMA DE nueva base de datos 
 function registro(){
     location.href="basedatos.html";
 }
+
 //ALUMNOS
 function verificarmatricula() {
     let matricula = document.getElementById('matricula').value;
@@ -31,10 +35,12 @@ function verificarmatricula() {
     alert('clave incorrecta');
     }
     }
+
 //REGISTRO DE ENTRADA DE ALUMNOS
 function Entradalumno(){
     location.href="entralumno.html"
 }
+
 //REGISTRO DE SALIDA DE ALUMNOS   
 function Salidalumno(){
     location.href="salidalumno.html"
